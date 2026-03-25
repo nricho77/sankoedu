aa17830e9bcf684fccbc1c6f7e42aefded15f9e6
nricho77 <86055650+nricho77@users.noreply.github.com>
2026-03-25 20:28:12 +0100

    Create function.js

---
+++ api/health/function.js
@@ -0,0 +1,14 @@
+{
+  "bindings": [
+    {
+      "authLevel": "anonymous",
+      "type": "httpTrigger",
+      "direction": "in",
+      "methods": ["get"]
+    },
+    {
+      "type": "http",
+      "direction": "out"
+    }
+  ]
+}
