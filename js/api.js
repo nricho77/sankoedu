03949ccca77f04fb9f2236f60db0c2c0fefa1cae
nricho77 <86055650+nricho77@users.noreply.github.com>
2026-03-26 14:22:17 +0100

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,10 +1,12 @@
 export const api = {
-  async health() {
-    const r = await fetch("/api/health");
-    return r.text();
-  },
   async login() {
     const r = await fetch("/api/login", { method: "POST" });
-    return { ok: r.ok };
+    if (!r.ok) throw new Error();
+    return r.text();
+  },
+
+  async getSharePointData() {
+    const r = await fetch("/api/sharepoint");
+    return r.json();
   }
 };
