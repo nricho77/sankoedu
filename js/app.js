5a4bf8b4896e2dce176d3b5124565ee3888240e8
nricho77 <86055650+nricho77@users.noreply.github.com>
2026-03-26 14:21:10 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,31 +1,19 @@
 import { router } from "./router.js";
+import { renderLogin } from "../ui/login.js";
 import { api } from "./api.js";
 
-const login = document.getElementById("login-screen");
-const app   = document.getElementById("app");
+const loginContainer = document.getElementById("login-screen");
+const app = document.getElementById("app");
 
-function showLogin() {
-  login.classList.remove("hidden");
-  app.classList.add("hidden");
-}
 function showApp() {
-  login.classList.add("hidden");
+  loginContainer.classList.add("hidden");
   app.classList.remove("hidden");
+  router.go("home");
 }
 
-document.getElementById("btn-login-local").onclick = async () => {
-  const r = await api.login();
-  if (r.ok) {
-    showApp();
-    router.go("home");
-  } else {
-    alert("Erreur connexion");
-  }
-};
+renderLogin(async () => {
+  await api.login();
+  showApp();
+});
 
-showLogin();
 router.init();
-
-// Test backend
-api.health().then(console.log);
-``
