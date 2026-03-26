620a3795e4a75598f9ced704b027e927c70ff055
nricho77 <86055650+nricho77@users.noreply.github.com>
2026-03-26 20:10:54 +0100

    Update db.js

--- js/db.js
+++ js/db.js
@@ -11,114 +11,114 @@ class DB {
   // ORGANISATION
   // -------------------------------------------------------
   getOrg() {
-    return API.get("/config/org");
+    return api.get("/config/org");
   }
 
   // -------------------------------------------------------
   // USERS
   // -------------------------------------------------------
   listUsers() {
-    return API.get("/users");
+    return api.get("/users");
   }
 
   createUser(data) {
-    return API.post("/users", data);
+    return api.post("/users", data);
   }
 
   updateUser(id, data) {
-    return API.put(`/users/${id}`, data);
+    return api.put(`/users/${id}`, data);
   }
 
   disableUser(id) {
-    return API.post(`/users/${id}/disable`);
+    return api.post(`/users/${id}/disable`);
   }
 
   resetPassword(id) {
-    return API.post(`/users/${id}/resetPassword`);
+    return api.post(`/users/${id}/resetPassword`);
   }
 
   // -------------------------------------------------------
   // SITES
   // -------------------------------------------------------
   listSites() {
-    return API.get("/sites");
+    return api.get("/sites");
   }
 
   createSite(data) {
-    return API.post("/sites", data);
+    return api.post("/sites", data);
   }
 
   updateSite(id, data) {
-    return API.put(`/sites/${id}`, data);
+    return api.put(`/sites/${id}`, data);
   }
 
   // -------------------------------------------------------
   // PERIODS
   // -------------------------------------------------------
   listPeriods() {
-    return API.get("/periods");
+    return api.get("/periods");
   }
 
   createPeriod(data) {
-    return API.post("/periods", data);
+    return api.post("/periods", data);
   }
 
   updatePeriod(id, data) {
-    return API.put(`/periods/${id}`, data);
+    return api.put(`/periods/${id}`, data);
   }
 
   // -------------------------------------------------------
   // ENTRIES
   // -------------------------------------------------------
   getEntries(periodId, siteId) {
-    return API.get(`/entries?period=${periodId}&site=${siteId}`);
+    return api.get(`/entries?period=${periodId}&site=${siteId}`);
   }
 
   getEntry(id) {
-    return API.get(`/entries/${id}`);
+    return api.get(`/entries/${id}`);
   }
 
   saveDraft(id, data) {
-    return API.put(`/entries/${id}`, data);
+    return api.put(`/entries/${id}`, data);
   }
 
   submitEntry(id) {
-    return API.post(`/entries/${id}/submit`);
+    return api.post(`/entries/${id}/submit`);
   }
 
   cancelSubmit(id) {
-    return API.post(`/entries/${id}/cancelSubmit`);
+    return api.post(`/entries/${id}/cancelSubmit`);
   }
 
   approveEntry(id, banqueGest) {
-    return API.post(`/entries/${id}/approve`, { banqueGest });
+    return api.post(`/entries/${id}/approve`, { banqueGest });
   }
 
   refuseEntry(id, comment) {
-    return API.post(`/entries/${id}/refuse`, { comment });
+    return api.post(`/entries/${id}/refuse`, { comment });
   }
 
   // -------------------------------------------------------
   // SUMMARIES / EXPORTS
   // -------------------------------------------------------
   summaryEmployee(periodId, userId) {
-    return API.get(`/admin/summary/employee?period=${periodId}&user=${userId}`);
+    return api.get(`/admin/summary/employee?period=${periodId}&user=${userId}`);
   }
 
   summarySite(periodId, siteId) {
-    return API.get(`/admin/summary/site?period=${periodId}&site=${siteId}`);
+    return api.get(`/admin/summary/site?period=${periodId}&site=${siteId}`);
   }
 
   summaryPeriod(periodId) {
-    return API.get(`/admin/summary/period?period=${periodId}`);
+    return api.get(`/admin/summary/period?period=${periodId}`);
   }
 
   summaryAll(periodId) {
-    return API.get(`/admin/summary/all?period=${periodId}`);
+    return api.get(`/admin/summary/all?period=${periodId}`);
   }
 
   exportCSV(periodId) {
-    return API.get(`/admin/export?period=${periodId}`);
+    return api.get(`/admin/export?period=${periodId}`);
   }
 }
 
